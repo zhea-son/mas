4db05f975f347b02dd8ee7140f9ca76d529db03b
Ranjan <khanalranjan43@gmail.com>
2023-05-29 07:35:30 +0545

    Setup done

---
+++ database/migrations/2023_05_29_012608_create_schedules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('doctor_id')->constrained('doctors')->cascadeOnDelete();
+            $table->foreignId('dept_id')->constrained('departments')->cascadeOnDelete();
+            $table->date('date');
+            $table->time('from');
+            $table->time('to');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
